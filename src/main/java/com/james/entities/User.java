ec8c6fa2fea127a713ea3566dc84fe17c8593f6a
jamesyburr <vint.james@gmail.com>
2016-07-20 14:19:37 -0400

    added some variables to entities, some annotations

--- src/main/java/com/james/entities/User.java
+++ src/main/java/com/james/entities/User.java
@@ -1,7 +1,52 @@
 package com.james.entities;
 
+import javax.persistence.*;
+
 /**
  * Created by jamesyburr on 7/20/16.
  */
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue
+    int id;
+
+    @Column(nullable = false)
+    String userName;
+
+    @Column(nullable = false)
+    String password;
+
+    public User() {
+    }
+
+    public User(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
--- src/main/java/com/james/services/TaskRepository.java
+++ src/main/java/com/james/services/TaskRepository.java
@@ -1,7 +1,10 @@
 package com.james.services;
 
+import com.james.entities.Task;
+import org.springframework.data.repository.CrudRepository;
+
 /**
  * Created by jamesyburr on 7/20/16.
  */
-public interface TaskRepository {
+public interface TaskRepository extends CrudRepository<Task, Integer> {
 }
