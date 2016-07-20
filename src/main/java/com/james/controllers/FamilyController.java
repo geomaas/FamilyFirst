b4a64e777887dac1ff39e650e56d2a247e8b1f26
Liz Favre <egfavre@gmail.com>
2016-07-20 19:38:36 -0400

    Merge branch 'master' of https://github.com/TeamRigLizJam/FamilyFirst
    # Conflicts:
    #	.gitignore
    #	.idea/compiler.xml
    #	.idea/misc.xml
    #	.idea/modules.xml
    #	build.gradle
    #	gradle/wrapper/gradle-wrapper.jar
    #	gradle/wrapper/gradle-wrapper.properties
    #	gradlew.bat
    #	public/.keep
    #	src/main/java/com/james/FamilyFirstApplication.java
    #	src/test/java/com/james/FamilyFirstApplicationTests.java

--- src/main/java/com/james/controllers/FamilyController.java
+++ src/main/java/com/james/controllers/FamilyController.java
@@ -1,10 +1,38 @@
 package com.james.controllers;
 
+import com.james.entities.User;
+import com.james.services.TaskRepository;
+import com.james.services.UserRepository;
+import com.james.utils.PasswordStorage;
+import org.h2.tools.Server;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+import java.io.FileNotFoundException;
+import java.sql.SQLException;
 
 /**
  * Created by jamesyburr on 7/20/16.
  */
 @RestController
 public class FamilyController {
+    @Autowired
+    UserRepository users;
+
+    @Autowired
+    TaskRepository tasks;
+
+    @PostConstruct
+    public void init() throws SQLException, FileNotFoundException, PasswordStorage.CannotPerformOperationException {
+        Server.createWebServer().start();
+        User user = new User();
+    }
+
+    @RequestMapping (path = "/", method = RequestMethod.GET)
+    public String placeholderFrontPage (){
+        return  "";
+    }
 }
