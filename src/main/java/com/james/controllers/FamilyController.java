059144f97ee90462ec95ef6597494d02f66011f6
jamesyburr <vint.james@gmail.com>
2016-07-20 13:52:25 -0400

    added entities, controllers, services and utils

---
+++ src/main/java/com/james/controllers/FamilyController.java
@@ -0,0 +1,10 @@
+package com.james.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by jamesyburr on 7/20/16.
+ */
+@RestController
+public class FamilyController {
+}
---
+++ src/main/java/com/james/services/UserRepository.java
@@ -0,0 +1,7 @@
+package com.james.services;
+
+/**
+ * Created by jamesyburr on 7/20/16.
+ */
+public interface UserRepository {
+}
