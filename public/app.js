f551cda28aad2bb001aaf36385801aad7ae4fc04
Riggan Hart <rigganhart@Riggans-MacBook.local>
2016-07-21 12:08:46 -0400

    login navs to care page

--- public/app.js
+++ public/app.js
@@ -82,7 +82,7 @@ module.exports = function(app){
 
 // this service will handle the user data
   app.factory('userService', ['$http','$location', function($http, $location){
-
+    
 
 
 
@@ -93,22 +93,15 @@ module.exports = function(app){
               method: 'POST',
               url: '/login',
               data: {
-                username: user,
+                userName: user,
                 password: pass,
               }
           }).then(function(response) {
             console.log("here is whats coming back", response );
-            // from APITunes:
-            // if(response.data.isArtist === true){
-            //   $location.path('/artist');
-            //   angular.copy(response.data, currentUser )
-            //   console.log(currentUser);
-            // }else if (response.data.isUser === true) {
-            //   $location.path('/guest');
-            //   angular.copy(response.data, currentUser )
-            //   console.log(currentUser);
-            //
-            // }
+            if(response.data === true){
+              $location.path('/tasks');
+            }
+
           })
       },
 
