ec35ba5071fe5209e1344539317a3e74df574b1d
Riggan Hart <rigganhart@Riggans-MacBook.local>
2016-07-21 12:45:03 -0400

    tasks GET request works

--- public/app.js
+++ public/app.js
@@ -23,10 +23,12 @@ module.exports = function(app){
 // this handles the task manager view
   app.controller('taskManagerController', ['$scope', 'userService', 'taskService', function($scope, userService, taskService){
 
+    $scope.taskList = taskService.getAllTasks();
 
-
-
-
+    $scope.add = function(){
+      console.log(`send task text ${$scope.taskText}`);
+      taskService.addTask();
+    }
 
   }]);
 };
@@ -64,13 +66,33 @@ module.exports = function(app){
 
 // this service will handle the task data
   app.factory('taskService', ['$http','$location', function($http, $location){
-
+      let allTasksList = [];
 
 
 
     return {
-
-
+      getAllTasks: function(){
+        console.log(`get tasks from server`);
+        $http({
+              method: 'GET',
+              url: '/tasks',
+          }).then(function(response) {
+            console.log(response);
+            // angular.copy(response.data, allSongList);
+          })
+          // console.log("allsongs arrar", allSongList);
+          // return allSonglist
+      },
+      addTask: function(){
+        console.log(`add a new task`);
+        $http({
+              method: 'POST',
+              url: '/new',
+          }).then(function(response) {
+            console.log(response);
+            // angular.copy(response.data, allSongList);
+          })
+      }
 
     };
 
